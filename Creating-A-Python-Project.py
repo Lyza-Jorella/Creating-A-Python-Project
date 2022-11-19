b628247efc2bc8095b5e2007c37cbd729abdeedf
Lyza-Jorella <[EMAIL]>
2022-11-19 23:36:46 +0800

    Added def Calculate to see the result in calculator

--- Creating-A-Python-Project.py
+++ Creating-A-Python-Project.py
@@ -18,11 +18,25 @@ def show(value):
     equation += value
     label_result.config(text=equation)
 
-    def clear():
-        global equation
-        equation = ""
-        label_result.config(text=equation)
 
+def clear():
+    global equation
+    equation = ""
+    label_result.config(text=equation)
+
+
+def calculate():
+    global equation
+    result = ""
+    if equation != "":
+        try:
+            result = eval(equation)
+            except:
+            result = "error"
+            equation = ""
+
+
+label_result.config(text=result)
 
 label_result = Label(root, width=25, height=2, text="", font=("arial", 30))
 label_result.pack()
@@ -40,7 +54,7 @@ Button(root, text="7", width=5, height=1, font=("arial", 30, "bold"), bd=1, fg="
        command=lambda: show("7")).place(x=10, y=200)
 Button(root, text="8", width=5, height=1, font=("arial", 30, "bold"), bd=1, fg="#fff", bg="#2a2d36",
        command=lambda: show("8")).place(x=150, y=200)
-Button(root, text="8", width=5, height=1, font=("arial", 30, "bold"), bd=1, fg="#fff", bg="#2a2d36",
+Button(root, text="9", width=5, height=1, font=("arial", 30, "bold"), bd=1, fg="#fff", bg="#2a2d36",
        command=lambda: show("9")).place(x=290, y=200)
 Button(root, text="-", width=5, height=1, font=("arial", 30, "bold"), bd=1, fg="#fff", bg="#2a2d36",
        command=lambda: show("-")).place(x=430, y=200)
